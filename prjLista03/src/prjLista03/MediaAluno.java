9ba4f05d5f034c8b8559d3e824347aec4d67f425
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-08-19 11:57:37 -0300

    Add files via upload

---
+++ prjLista03/src/prjLista03/MediaAluno.java
@@ -0,0 +1,19 @@
+package prjLista03;
+   import java.util.Scanner;
+   
+public class MediaAluno {
+    public static void main(String args[]) {
+    	Scanner ler = new Scanner(System.in);
+    	double nota1, nota2, media;
+    	
+    	System.out.println("Digite a nota 1");
+    	nota1 = ler.nextDouble();
+    	
+    	System.out.println("Digite a nota 2");
+    	nota2= ler.nextDouble();
+    	
+    	media=(nota1+nota2)/2;
+    	System.out.println("Sua média é="+media);
+    	ler.close();
+   }
+}
