5806b0deb11abab1be2fccad41d8823708d7381f
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-08-19 11:55:06 -0300

    Add files via upload

---
+++ Lista01/src/AloMundo.java
@@ -0,0 +1,8 @@
+
+public class AloMundo {
+   public static void main(String args[]) {
+	   //escreva("Ola Mundo")
+	   System.out.println("Hello World");
+	   System.out.println("Olá Mundo");
+   }
+}
