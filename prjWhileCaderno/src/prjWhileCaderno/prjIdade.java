c892ea710f0fa654bb3b2c0412b772688f475f5f
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-09-22 14:50:23 -0300

    Add files via upload

--- prjWhileCaderno/src/prjWhileCaderno/prjIdade.java
+++ prjWhileCaderno/src/prjWhileCaderno/prjIdade.java
@@ -20,6 +20,7 @@ public class prjIdade {
 				}
 			i++;
 		}
+		ler.close();
 	}
 	
 }
--- prjWhileCaderno/src/prjWhileCaderno/prjsomatoria.java
+++ prjWhileCaderno/src/prjWhileCaderno/prjsomatoria.java
@@ -1,5 +1,19 @@
 package prjWhileCaderno;
+import java.util.Scanner;
 
 public class prjsomatoria {
-
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		int i=0, r=0;
+			while (i<=100) {
+				r +=i;
+				i++;
+				System.out.println(r);
+			
+				
+			}
+			ler.close();
+	}
 }
+
+
