68ebe7643dbaa8941e63f0d00f0abdd7b9bf4778
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-09-23 11:17:41 -0300

    Delete prjsesenao/DataAno/1Bimestre directory

--- prjsesenao/DataAno/1Bimestre/1aula/AloMundo.java
+++
@@ -1,8 +0,0 @@
-
-public class AloMundo {
-   public static void main(String args[]) {
-	   //escreva("Ola Mundo")
-	   System.out.println("Hello World");
-	   System.out.println("Olá Mundo");
-   }
-}
