5f9440397b5b4fcf8339a1fb37360b44c2a241f3
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-09-23 10:45:40 -0300

    Add files via upload

---
+++ 3Bimestre/WhileCaderno/prjsomatoria.java
@@ -0,0 +1,16 @@
+package prjWhileCaderno;
+
+public class prjsomatoria {
+	public static void main(String[] args) {
+		int i=0, r=0;
+			while (i<=100) {
+				r +=i;
+				i++;
+				System.out.println(r);
+			
+				
+			}
+	}
+}
+
+
