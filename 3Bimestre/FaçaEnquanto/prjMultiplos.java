b47812a26bd9e7c5985cb4a4229b6aff1c1c9f77
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-09-29 14:40:21 -0300

    Delete 3Bimestre/FaçaEnquanto/prjMultiplos.java

--- 3Bimestre/FaçaEnquanto/prjMultiplos.java
+++
@@ -1,9 +0,0 @@
-package prjFor;
-
-public class prjMultiplos {
-	public static void main(String[] args) { ;
-		for(int i=0;i<=100; i=i+10) {
-			System.out.println(i+" - é Múltiplo de 10");
-		}
-	}
-}
