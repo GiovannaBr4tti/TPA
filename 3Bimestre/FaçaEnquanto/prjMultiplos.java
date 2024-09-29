3680611e34bc79d99cdbc921b01f26dac9e2a85b
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-09-29 14:41:09 -0300

    Add files via upload

---
+++ 3Bimestre/FaçaEnquanto/prjMultiplos.java
@@ -0,0 +1,12 @@
+package prjFor;
+
+public class prjMultiplos {
+	public static void main(String[] args) { ;
+		for(int i=1;i<=100; i++) {
+			System.out.println(i);
+			if(i%10==0) {
+				System.out.println("É múltiplo de 10");
+			}
+		}
+	}
+}
