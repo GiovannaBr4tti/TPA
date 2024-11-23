0443ea9ff589a9d68e156ac2598873c311e4849b
GiovannaBr4tti <giovannabratti12@gmail.com>
2024-11-23 13:58:51 -0300

    Add files via upload

---
+++ 4Bimestre/Lista15Exercicios/prjVetorPrimo.java
@@ -0,0 +1,42 @@
+package prjListaVetoresAvaliativa;
+import java.util.Scanner;
+public class prjVetorPrimo {
+		public static void main(String args[]) {
+			Scanner ler = new Scanner (System.in);
+			final int TAM=10;
+			int i, j, divisor;
+			int a[] = new int[TAM];
+			
+			System.out.println("Digite 10 números inteiros para o vetor a:");
+			for(i=0; i<TAM; i++) {
+				System.out.println("Digite o "+(i+1)+" numero: ");
+				a[i]= ler.nextInt();
+			}
+			
+			
+			
+			for (i = 0; i < TAM; i++) {
+		            int n = a[i]; 
+		            divisor = 0; 
+		            
+		    if (n < 2) {
+		        System.out.println(n + " não é primo.");
+		        System.out.println();
+			}else {
+				for(j=2; j<a[i]; j++) {
+					if (a[i] % j==0) {
+						divisor = 1;
+					} 
+				}
+				if(divisor==0) {
+					System.out.println(n +" é primo.");
+					System.out.println();
+				}else {
+					System.out.println(n +" não é primo.");
+					System.out.println();
+				}
+					
+			   }
+			}
+		}
+	}
